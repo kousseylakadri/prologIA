f6d75ab74f62f555b20875346a8d252a96d3fe72
Vicente Guerra <vicenteguerrahdez@gmail.com>
2015-11-24 20:09:16 -0600

    lineas impares

--- bdc.pl
+++ bdc.pl
@@ -1,11 +1,118 @@
-%% linea(numero,nombre,conexion,tipo).
-%% conexion:
-%%  normal
-%%  transbordo
-%% tipo:
-%%  paso
-%%  terminal
+%%_linea(numero,nombre,conexion,tipo).
+%%_conexion:
+%%__normal
+%%__transbordo
+%%_tipo:
+%%__paso
+%%__terminal
 
-%% David Pares / Vicente Impares
+%%_David_Pares_/_Vicente_Impares
 
 linea(1,pantitlán,transbordo,terminal).
+linea(1,zaragoza,normal,normal).
+linea(1,gómez_farías,normal,normal).
+linea(1,boulevard_puerto_aéreo,normal,normal).
+linea(1,balbuena,normal,normal).
+linea(1,moctezuma,normal,normal).
+linea(1,san_lázaro,transbordo,normal).
+linea(1,candelaria,transbordo,normal).
+linea(1,merced,normal,normal).
+linea(1,pino_suárez,transbordo,normal).
+linea(1,isabel_la_católica,normal,normal).
+linea(1,salto_del_agua,transbordo,normal).
+linea(1,balderas,transbordo,normal).
+linea(1,cuauhtémoc,normal,normal).
+linea(1,insurgentes,normal,normal).
+linea(1,sevilla,normal,normal).
+linea(1,chapultepec,normal,normal).
+linea(1,juanacatlán,normal,normal).
+linea(1,tacubaya,transbordo,normal).
+linea(1,observatorio,normal,terminal).
+
+linea(3,indios_verdes,normal,terminal).
+linea(3,deportivo_18_de_marzo,transbordo,normal).
+linea(3,potrero,normal,normal).
+linea(3,la_raza,transbordo,normal).
+linea(3,tlatelolco,normal,normal).
+linea(3,guerrero,transbordo,normal).
+linea(3,hidalgo,transbordo,normal).
+linea(3,juárez,normal,normal).
+linea(3,balderas,transbordo,normal).
+linea(3,niños_héroes,normal,normal).
+linea(3,hospital_general,normal,normal).
+linea(3,centro_médico,transbordo,normal).
+linea(3,etiopía-plaza_de_la_transparencia,normal,normal).
+linea(3,eugenia,normal,normal).
+linea(3,división_del_norte,normal,normal).
+linea(3,zapata,transbordo,normal).
+linea(3,coyoacán,normal,normal).
+linea(3,viveros-derechos_humanos,normal,normal).
+linea(3,miguel_ángel_de_quevedo,normal,normal).
+linea(3,copilco,normal,normal).
+linea(3,universidad,normal,terminal).
+
+
+linea(5,pantitlán,transbordo,terminal).
+linea(5,hangares,normal,normal).
+linea(5,terminal_aérea,normal,normal).
+linea(5,oceanía,transbordo,normal).
+linea(5,aragón,normal,normal).
+linea(5,eduardo_molina,normal,normal).
+linea(5,consulado,transbordo,normal).
+linea(5,valle_gómez,normal,normal).
+linea(5,misterios,normal,normal).
+linea(5,la_raza,transbordo,normal).
+linea(5,autobuses_del_norte,normal,normal).
+linea(5,instituto_del_petróleo,transbordo,normal).
+linea(5,politécnico,normal,terminal).
+
+linea(7,el_rosario,transbordo,terminal).
+linea(7,aquiles_serdán,normal,normal).
+linea(7,refinería,normal,normal).
+linea(7,tacuba,transbordo,normal).
+linea(7,san_joaquín,normal,normal).
+linea(7,polanco,normal,normal).
+linea(7,auditorio,normal,nom).
+linea(7,constituyentes,normal,normal).
+linea(7,tacubaya,transbordo,normal).
+linea(7,san_pedro_de_los_pinos,normal,normal).
+linea(7,san_antonio,normal,normal).
+linea(7,mixcoac,transbordo,normal).
+linea(7,barranca_del_muerto,normal,terminal).
+
+linea(9,pantitlán,transbordo,terminal).
+linea(9,puebla,normal,normal).
+linea(9,iztacalco,normal,normal).
+linea(9,ciudad_deportiva,normal,normal).
+linea(9,velódromo,normal,normal).
+linea(9,mixiuhca,normal,normal).
+linea(9,jamaica,transbordo,normal).
+linea(9,chabacano,transbordo,normal).
+linea(9,lázaro_cárdenas,normal,normal).
+linea(9,centro_médico,transbordo,normal).
+linea(9,chilpancingo,normal,normal).
+linea(9,patriotismo,normal,normal).
+linea(9,tacubaya,transbordo,terminal).
+
+linea(B,Buenavista,transbordo,terminal).
+linea(B,Guerrero,transbordo,normal).
+linea(B,Garibaldi-Lagunilla,normal,normal).
+linea(B,Lagunilla,normal,normal).
+linea(B,Tepit,normal,normal).
+linea(B,Morelos,transbordo,normal).
+linea(B,San_Lázaro,transbordo,normal).
+linea(B,Ricardo_Flores_Magón,normal,normal).
+linea(B,Romero_Rubi,normal,normal).
+linea(B,Oceanía	Gustavo_A._Mader,transbordo,normal).
+linea(B,Deportivo_Oceaní,normal,normal).
+linea(B,Bosque_de_Aragó,normal,normal).
+linea(B,Villa_de_Aragó,normal,normal).
+linea(B,Nezahualcóyot,normal,normal).
+linea(B,Impulsor,normal,normal).
+linea(B,Río_de_los_Remedio,normal,normal).
+linea(B,Múzquiz	Ecatepec_de_Morelo,normal,normal).
+linea(B,Ecatepe,normal,normal).
+linea(B,Olímpic,normal,normal).
+linea(B,Plaza_Aragó,normal,normal).
+linea(B,Ciudad_Aztec,normal,terminal).
+
